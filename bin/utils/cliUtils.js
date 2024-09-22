89336742e2a368b67b1c1536edbf3fb23a785f55
brady bridges <bradyjbridges@gmail.com>
2024-09-22 14:54:29 -0500

    Add command to add new task via CLI

--- bin/utils/cliUtils.js
+++ bin/utils/cliUtils.js
@@ -1,6 +1,11 @@
 /* external imports */
 import * as commander from 'commander'
 
+import {
+	printErrorMessage,
+	printSuccessMessage,
+} from './printUtils.js'
+
 const programInformation = {
 	name: 'todo-cli',
 	description: 'A command line tool to manage a simple todo list.',
@@ -18,10 +23,26 @@ const initProgramInformation = (program, todoManager) => {
 		)
 }
 
+const initAddTaskCommand = (program, todoManager) => {
+	program
+		.command('add')
+		.description('Add a new task')
+		.argument('<string>', 'New task')
+		.action((newTodo) => {
+			if (newTodo && typeof newTodo === 'string') {
+				todoManager.addTodo(newTodo)
+				printSuccessMessage(`"${newTodo}" added successfully`)
+			} else {
+				printErrorMessage('Invalid task. String expected')
+			}
+		})
+}
+
 const initCLI = (todoManager) => {
 	const program = new commander.Command()
 
 	initProgramInformation(program, todoManager)
+	initAddTaskCommand(program, todoManager)
 	return program
 }
 
