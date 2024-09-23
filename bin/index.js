f85a424d75b2136f639f40ec3d1b52279b1e578b
brady bridges <bradyjbridges@gmail.com>
2024-09-22 20:00:50 -0500

    Move GUI functionality to dedicated file

--- bin/index.js
+++ bin/index.js
@@ -1,41 +1,10 @@
 #! /usr/bin/env node
 
 import { initCLI } from '../utils/cli.js'
-import { TodoManager } from '../utils/TodoManager.js'
-import { printBox, printTitle, printTodoList } from '../utils/print.js'
-import { getMenuSelectionPrompt } from '../utils/prompts.js'
-import { handleMenuSelection } from '../utils/actions.js'
-
-const todoManager = new TodoManager()
+import { displayGUI } from '../utils/gui.js'
 
 if (process.argv.length > 2) {
 	initCLI()
 } else {
-	let runApplication = true
-
-	while (runApplication) {
-		try {
-			if (todoManager.todos.length) printTitle(todoManager.todos)
-			printTodoList(todoManager.todos)
-
-			const menuSelection = await getMenuSelectionPrompt()
-
-			if (!menuSelection) {
-				runApplication = false
-			}
-
-			await handleMenuSelection(menuSelection, todoManager)
-		} catch (e) {
-			printBox(
-				e,
-				{
-					title: 'Exiting due to an error',
-					titleAlignment: 'center',
-					borderColor: 'red',
-				},
-				'error'
-			)
-			runApplication = false
-		}
-	}
+	displayGUI()
 }
---
+++ utils/gui.js
@@ -0,0 +1,35 @@
+import { TodoManager } from '../utils/TodoManager.js'
+import { printBox, printTitle, printTodoList } from '../utils/print.js'
+import { getMenuSelectionPrompt } from '../utils/prompts.js'
+import { handleMenuSelection } from '../utils/actions.js'
+
+export const displayGUI = async () => {
+	let displayGUI = true
+	const todoManager = new TodoManager()
+
+	while (displayGUI) {
+		try {
+			if (todoManager.todos.length) printTitle(todoManager.todos)
+			printTodoList(todoManager.todos)
+
+			const menuSelection = await getMenuSelectionPrompt()
+
+			if (!menuSelection) {
+				displayGUI = false
+			}
+
+			await handleMenuSelection(menuSelection, todoManager)
+		} catch (e) {
+			printBox(
+				e,
+				{
+					title: 'Exiting due to an error',
+					titleAlignment: 'center',
+					borderColor: 'red',
+				},
+				'error'
+			)
+			displayGUI = false
+		}
+	}
+}
