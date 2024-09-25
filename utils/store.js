e35f29bc882a63f7fec7503dee1391715b856099
brady bridges <bradyjbridges@gmail.com>
2024-09-24 20:28:54 -0500

    Add initial default settings to allow users to configure preferences

--- utils/store.js
+++ utils/store.js
@@ -1,4 +1,3 @@
-
 import Conf from 'conf'
 
 const storeConfig = {
@@ -7,6 +6,16 @@ const storeConfig = {
 		todos: {
 			type: 'array',
 			default: [],
+		},
+		settings: {
+			type: 'object',
+			default: {
+				disableExitMessage: false,
+				exitErrorMessage: 'Goodbye',
+				exitMessage: 'Godspeed, friend',
+				headerTitle: 'TODO LIST',
+				noTasksMessage: 'No TODOs found. Take the day off.',
+			},
 		},
 	},
 }
