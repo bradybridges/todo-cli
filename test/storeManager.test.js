246be396b3d2b9b55c9cd581df546c7bb9641863
brady <bradyjbridges@gmail.com>
2024-09-27 14:31:28 -0500

    Add tests for settings update/restore functionality

--- test/storeManager.test.js
+++ test/storeManager.test.js
@@ -1,17 +1,24 @@
 import { expect, assert } from 'chai'
 import { StoreManager } from '../utils/StoreManager.js'
+import { defaultStoreSettings } from '../utils/store.js'
 
-describe('Todo Manager', () => {
+describe('Store Manager', () => {
 	const storeManager = new StoreManager()
 
-	it('should initialize with an empty todo list', () => {
+	it('it should initialize with an empty todo list', () => {
 		const todos = storeManager.todos
 
-		assert.isArray(todos, 'Todo list is an array')
+		assert.isArray(todos, 'todo list is an array')
 		expect(todos).to.have.lengthOf(0)
 	})
 
-	it('should be able to add a new task', () => {
+	it('it should initialize with default settings', () => {
+		const settings = storeManager.settings
+		assert.isObject(settings, 'settings is an object')
+		expect(settings).to.eql(defaultStoreSettings)
+	})
+
+	it('it should be able to add a new task', () => {
 		const todos = storeManager.todos
 		const todoLabel = 'test'
 		storeManager.addTodo(todoLabel)
@@ -21,7 +28,7 @@ describe('Todo Manager', () => {
 		expect(todos[0].complete).to.equal(false)
 	})
 
-	it('should be able to update tasks', () => {
+	it('it should be able to update tasks', () => {
 		const updatedTodos = [
 			{ label: '1', complete: false },
 			{ label: '2', complete: false },
@@ -36,12 +43,36 @@ describe('Todo Manager', () => {
 		expect(todos[2].complete).to.equal(true)
 	})
 
-	it('should be able to delete all tasks', () => {
+	it('it should be able to delete all tasks', () => {
 		const todos = storeManager.todos
 		expect(todos).to.have.lengthOf(3)
 
 		storeManager.deleteTodos()
 		assert.isArray(storeManager.todos)
 		expect(storeManager.todos).to.have.lengthOf(0)
+	})
+
+	it('should be able to update settings', () => {
+		const disableExitMessage = true
+		const exitErrorMessage = 'test exit error message'
+		const exitMessage = 'test exit message'
+		const headerTitle = 'test header title'
+		const noTasksMessage = 'test no tasks message'
+
+		const testSettings = {
+			disableExitMessage,
+			exitErrorMessage,
+			exitMessage,
+			headerTitle,
+			noTasksMessage,
+		}
+
+		storeManager.updateSettings(testSettings)
+		expect(storeManager.settings).to.eql(testSettings)
+	})
+
+	it('should be able to restore default settings', () => {
+		storeManager.restoreDefaultSettings()
+		expect(storeManager.settings).to.eql(defaultStoreSettings)
 	})
 })
