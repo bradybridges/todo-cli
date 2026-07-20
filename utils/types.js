f0b09749b62344aea9f5e933cdb67c3e8901ceb1
Brady <bradyjbridges@gmail.com>
2026-07-19 21:01:56 -0500

    Add JSDoc support

---
+++ utils/types.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {Object} Todo
+ * @property {string} label
+ * @property {boolean} complete
+ */
+
+/**
+ * @typedef {Object} Settings
+ * @property {boolean} disableExitMessage
+ * @property {string} exitErrorMessage
+ * @property {string} exitMessage
+ * @property {string} headerTitle
+ * @property {string} noTasksMessage
+ */
+
+/**
+ * @typedef {Object} StoreSchema
+ * @property {Todo[]} todos
+ * @property {Settings} settings
+ */
